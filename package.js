4ac05f5627021a4db6a975ab6bfa43cdf571dcde
tstikvoort <tobias@q42.nl>
2015-11-09 09:48:36 +0100

    first version of preloadimage

---
+++ package.js
@@ -0,0 +1,27 @@
+Package.describe({
+  name: 'tstikvoort:preloadimage',
+  version: '0.0.7',
+  // Brief, one-line summary of the package.
+  summary: 'A supersimple imagepreloader',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.use('ecmascript');
+  api.use('reactive-var');
+  api.addFiles('preloadimage.js');
+  api.export('preloadImage', ['client', 'server']);
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('tstikvoort:preloadimage');
+  api.addFiles('preloadimage-tests.js');
+  api.export('preloadImage', ['client', 'server']);
+});
